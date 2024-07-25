e81db98209487ca319d8169758c874ba64f28a7e
lsiwh37249 <lsiwh37249@gmail.com>
2024-07-25 14:37:07 +0900

    기능 범위를 벗어나면 사용법을 출력

--- src/lah/cli.py
+++ src/lah/cli.py
@@ -18,9 +18,14 @@ def cmd():
 
     if args.scount:
         print(f"-s => {args.scount}")
+        # TODO 명령어 카운트
     elif args.top:
         print(f"-t => {args.top}")
         if args.dt:
             print(f"-d => {args.dt}")
+            #TODO 특정 날짜의 명령어 TOP N
         else:
-            print("TODO")
+            parser.print_help()
+    else:
+        # TODO - 사용법을 출력한다. 
+        parser.print_help()
